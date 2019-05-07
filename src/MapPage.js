87593a88a3693fab09706b61be99a52aa46fcadc
Alin Saenchaichana <alin.scn@gmail.com>
2019-05-07 23:23:39 +0900

    test bin marker

--- src/MapPage.js
+++ src/MapPage.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const markerStyle = {
+  position: 'absolute',
+  width: 40,
+  height: 40,
+  left: -20,
+  top: -20,
+
+  border: '5px solid #f44336',
+  borderRadius: 40,
+  backgroundColor: 'white',
+  textAlign: 'center',
+  color: '#3f51b5',
+  fontSize: 16,
+  fontWeight: 'bold',
+  padding: 4
+};
+
+class BinMarker extends Component {
+
+  static defaultProps = {};
+
+  render(){
+    return(
+      <div style={markerStyle}>
+        {this.props.text}
+      </div>
+    );
+  }
+}
 
 class MapPage extends Component {
   static defaultProps = {
@@ -19,7 +47,12 @@ class MapPage extends Component {
           bootstrapURLKeys={{ key: 'AIzaSyCv7aQ0qD19jSxd954UZSZVQSDXZr1cNLs'}}
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
-        ></GoogleMapReact>
+        >
+          <BinMarker
+            lat={59.955413}
+            lng={30.337844}
+            text='test'/>
+        </GoogleMapReact>
       </div>
     );
   }
