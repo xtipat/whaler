4aac500af7120634863d31e6449cbcd892c75267
Alin Saenchaichana <alin.scn@gmail.com>
2019-05-08 21:57:45 +0900

    tabs in modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,34 +3,42 @@ import Modal from 'react-bootstrap/Modal'
 import Nav from 'react-bootstrap/Nav'
 import Tab from 'react-bootstrap/Tab'
 import '../assets/scss/binDetails.scss';
+import '../assets/scss/_base.scss';
 
 class BinDetails extends React.Component {
   render() {
     return (
       <Modal
+        size="sm"
         {...this.props}
-        size="lg"
         centered
       >
-        <Modal.Header closeButton>
-          <Nav variant="pills" defaultActiveKey="location">
-            <Nav.Item bsClass="tabs">
-              <Nav.Link eventKey="location">Location</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="details">Details</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="results">Results</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Modal.Header>
-        <Modal.Body>
-
-        </Modal.Body>
-        <Modal.Footer>
-          <button onClick={this.props.onHide}>Close</button>
-        </Modal.Footer>
+        <Tab.Container defaultActiveKey="location">
+          <Modal.Header closeButton>
+            <Nav fill variant="pills">
+              <Nav.Item>
+                <Nav.Link eventKey="location">Location</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="details">Details</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="results">Results</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Modal.Header>
+          <Modal.Body>
+            <Tab.Content>
+              <Tab.Pane eventKey="location">
+                Loca
+              </Tab.Pane>
+              <Tab.Pane eventKey="details">
+              </Tab.Pane>
+              <Tab.Pane eventKey="results">
+              </Tab.Pane>
+            </Tab.Content>
+          </Modal.Body>
+        </Tab.Container>
       </Modal>
     );
   }
