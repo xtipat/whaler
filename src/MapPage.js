39f45495e524b63aa71783d24f0218caec38de89
mdechdee <dechdee.m@gmail.com>
2019-05-08 19:12:49 +0900

    Add initial pin marks on Map page
    - Pin read locations from database and mark it on the map

--- src/MapPage.js
+++ src/MapPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 import BinMarker from './components/BinMarker.js';
-
+import {db} from './firebase/firebase.js';
 class MapPage extends Component {
   static defaultProps = {
     center: {
@@ -12,8 +12,54 @@ class MapPage extends Component {
     exampleMapStyles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]
   };
 
+  constructor(props){
+    super(props);
+    this.state = {
+        loading: null,
+        markers: []
+    };
+  };
+
+  fetchBinsData(){
+    this.setState({
+      loaded: false,
+      loading: true
+    });
+    db.ref(`bins`).once('value').then(snapshot => {
+      snapshot.forEach((child) => {
+        this.setState({
+          markers: this.state.markers.concat(child.val().location)
+        });
+      });
+      this.setState({
+      loading: false,
+      loaded: true
+      })
+      }
+    );
+  }
+
+  componentDidMount() {
+    this.fetchBinsData();
+  }
+
+  markBins(){
+    let markers = [];
+    for(let i = 0;i<this.state.markers.length;i++)
+    {
+      markers.push(
+        <BinMarker
+        lat={this.state.markers[i].lat}
+        lng={this.state.markers[i].lng}/>
+      );
+    }
+    return markers
+  }
+
   render() {
-    return (
+    if(this.state.loaded)
+    {
+      return (
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyCv7aQ0qD19jSxd954UZSZVQSDXZr1cNLs'}}
@@ -21,12 +67,16 @@ class MapPage extends Component {
           defaultZoom={this.props.zoom}
           options={{styles :this.props.exampleMapStyles}}
         >
-          <BinMarker
-            lat={59.955413}
-            lng={30.337844}/>
+        {this.markBins()}
         </GoogleMapReact>
       </div>
-    );
+      );
+    }
+    else 
+    {
+      //REPLACE <div/> WITH LOADING SCREEN
+        return(<div/>);
+    }
   }
 }
 
