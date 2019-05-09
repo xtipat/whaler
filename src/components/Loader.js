28b16ee9c0f0e75fe0f9131a9e1b409b651fd9ef
prethrolic <re.prestige@windowslive.com>
2019-05-10 07:54:56 +0900

    remove unused component

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../assets/scss/_base.scss';
 
 class Loader extends React.Component {
-  constructor(props){
-    super(props)
-  }
   render(){
     var color = '#17181F';
     if (this.props.white) color = 'white';
