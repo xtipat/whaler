422dce4f0f6a2557c6e56ab85a63ed8604527fa5
mdechdee <dechdee.m@gmail.com>
2019-05-10 01:40:13 +0900

    Add initial position on map using GPS

--- src/MapPage.js
+++ src/MapPage.js
@@ -10,7 +10,7 @@ class MapPage extends Component {
       lat: 59.95,
       lng: 30.33
     },
-    zoom: 11,
+    zoom: 15,
     exampleMapStyles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"color":"#000000"},{"lightness":13}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#144b53"},{"lightness":14},{"weight":1.4}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#08304b"}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#0c4152"},{"lightness":5}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#0b434f"},{"lightness":25}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#000000"}]},{"featureType":"road.arterial","elementType":"geometry.stroke","stylers":[{"color":"#0b3d51"},{"lightness":16}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"}]},{"featureType":"transit","elementType":"all","stylers":[{"color":"#146474"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#021019"}]}]
   };
 
@@ -18,11 +18,30 @@ class MapPage extends Component {
     super(props);
     this.state = {
         loading: null,
-        markers: []
+        markers: [],
+        lat: this.props.center.lat,
+        lng: this.props.center.lng,
+        gotLoc: false
     };
   };
-
+  getGeoLocation(){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+          position => {
+          this.setState({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+            gotLoc: true
+          });
+        }
+      )
+    } else {
+      console.log(1)
+      this.setState({gotLoc: true});
+    }
+  }
   fetchAllBinsData(){
+    this.getGeoLocation()
     this.setState({
       loaded: false,
       loading: true
@@ -64,13 +83,13 @@ class MapPage extends Component {
   }
 
   render() {
-    if(this.state.loaded)
+    if(this.state.loaded && this.state.gotLoc)
     {
       return (
       <div style={this.props.divSize}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyCv7aQ0qD19jSxd954UZSZVQSDXZr1cNLs'}}
-          defaultCenter={this.props.center}
+          defaultCenter={{lat:this.state.lat,lng:this.state.lng}}
           defaultZoom={this.props.zoom}
           options={{styles :this.props.exampleMapStyles}}
         >
