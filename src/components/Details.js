d5e8f3ecf1afcdba7911610f4e225ad1e7371fc6
prethrolic <re.prestige@windowslive.com>
2019-05-14 23:05:51 +0900

    edit detail style

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom"
 import Title from "./Title";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Modal, Nav, Tab, Button, ProgressBar } from 'react-bootstrap';
+import '../assets/scss/redeem.scss';
 
 
 
@@ -13,24 +14,18 @@ export class Details extends Component {
 			<ProductConsumer >
 				{(value) =>{
 					const {
-						id, 
-						company, 
-						img, 
+						id,
+						company,
+						img,
 						info,
-						price, 
+						price,
 						title,
 						inCart
 					} = value.detailProduct;
 					const user_point = value.user_point
 					return(
-						<div className="container py-5" style={{ overflowY: 'scroll', height: '90vh' }}>
-							<ProductConsumer>
-							{(value)=>{
-								return (<Title name="Redeem" title={value.user_point}/>)
-								
-							}}
-							</ProductConsumer>
-				
+						<div className='detail-wrap'>
+
 							{/*product info*/}
 							<div className="row">
 								<div className="col-10 mx-auto col-md-10 my-3">
@@ -40,7 +35,7 @@ export class Details extends Component {
 								<div className="col-12 mx-auto col-md-12 my-3 text-capitalize">
 									<h2>{title}</h2>
 									<h4 className="text-title text-uppercase text-mutate mt-3 mb-2">
-										by : <span className="text-uppercase">{company}</span> 
+										by : <span className="text-uppercase">{company}</span>
 									</h4>
 									<h4 className="text-gold">
 										<strong>
@@ -53,11 +48,11 @@ export class Details extends Component {
 									<p className="text-muted lead">{info}</p>
 								{/*button*/}
 								<div>
-									
+
 									<div style={{textAlign: 'right'}}>
-          								<Button cart="true" 
-          										variant="yellow" 
-          										disabled={(user_point < price)? true:false} 
+          								<Button cart="true"
+          										variant="yellow"
+          										disabled={(user_point < price)? true:false}
           										onClick={()=>{
 															value.openModal(id);
 												}}>
@@ -68,26 +63,19 @@ export class Details extends Component {
           									<Button variant="black"><FontAwesomeIcon icon='times-circle'/> Go Back</Button>
         								</Link>
         							</div>
-								
+
 								</div>
 
-									
+
 								</div>
 							</div>
-						</div>
-						)
+					</div>
+					)
 				}}
-				
+
 			</ProductConsumer>
 		);
 	}
 }
 
 export default Details;
-
-
-
-
-
-
-
