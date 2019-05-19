67552e633b0c27a84ba2528a044ad7d2e6264b46
prethrolic <re.prestige@windowslive.com>
2019-05-20 05:31:36 +0900

    change titlefont from Lato to Oswald

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Menubar from './Menubar.js';
 import Profile from './Profile.js'
 import Redeem from './Redeem.js';
@@ -8,9 +8,12 @@ import Details from './components/Details';
 import ModalRedeem from './components/ModalRedeem';
 import Signin from './authentication/Signin.js';
 import Signup from './authentication/Signup.js';
+import Loader from './components/Loader.js'
+import withAuthentication from './session/withAuthentication.js';
+import AuthUserContext from './session/authUserContext';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-bootstrap/dist/react-bootstrap.min.js';
 import './assets/scss/_base.scss';
-import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 //import GoogleMapReact from 'google-map-react';
 import MapPage from './MapPage.js'
@@ -26,19 +29,58 @@ library.add(faTrash, faTimesCircle, faCheckCircle, faMapMarkedAlt, faSearch,
   faGift, faUserCircle, faPlusCircle, faMapPin, faCompass, faTimes, faCircleNotch,
   faMale, faMapMarkerAlt, faInfoCircle, faPollH, faFish);
 
-function App() {
-  return (
-    <Container fluid>
-      <Row className='justify-content-sm-center outer-wrap'>
-        <Col xs={12} sm={8} md={6} lg={4} className='page-wrap'>
-          <Page />
-          <ModalRedeem/>
-          <ToastContainer position={toast.POSITION.TOP_CENTER} />
-          <Menubar />
-        </Col>
-      </Row>
-    </Container>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true,
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({loading: false});
+    }, 400)
+  }
+
+  render(){
+    return (
+      <Container fluid>
+        <Row className='justify-content-sm-center outer-wrap'>
+            <AuthUserContext.Consumer>
+            {authUser => {
+              if(authUser){
+                return(
+                  <Col xs={12} sm={8} md={6} lg={4} className='page-wrap'>
+                    <Page />
+                    <ModalRedeem/>
+                    <ToastContainer position={toast.POSITION.TOP_CENTER} />
+                    <Menubar />
+                  </Col>
+                );
+              } else {
+                if(this.state.loading){
+                  return(
+                  <Col xs={12} sm={8} md={6} lg={4} className='nonauth-wrap'>
+                    <Loader primary />
+                  </Col>
+                  )
+                }
+                else {
+                  return(
+                  <Col xs={12} sm={8} md={6} lg={4} className='nonauth-wrap'>
+                    <NonAuth />
+                  </Col>
+                );
+                }
+              }
+            }
+            }
+            </AuthUserContext.Consumer>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 const Page = () => (
@@ -49,9 +91,19 @@ const Page = () => (
       <Route path='/profile' component={Profile} />
       <Route path='/locate' component={Locate} />
       <Route path='/details' component={Details}/>
-      <Route path='/signin' component={Signin} />
-      <Route path='/signup' component={Signup} />
+      <Route exact path='/signin' render={() => (
+						<Redirect to='/' />
+			)}/>
+      <Route exact path='/signup' render={() => (
+						<Redirect to='/' />
+			)}/>
   </Switch>
 )
 
-export default withRouter(App);
+const NonAuth = () => (
+  <Switch>
+    <Route path='/signin' component={Signin} />
+    <Route path='/signup' component={Signup} />
+  </Switch>
+)
+export default withRouter(withAuthentication(App));
