152d690d6efae26184c25caca78a2dc43579a971
Chitipat Phetmunee <xtipat@gmail.com>
2019-05-27 11:45:34 +0900

    finish point redeem

--- src/Redeem.js
+++ src/Redeem.js
@@ -3,13 +3,15 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import ProductList from './components/ProductList';
 import './assets/scss/redeem.scss';
+import {ProductConsumer} from "./context";
+import {firebase,db } from './firebase/firebase';
 
 
 
 
 function Redeem() {
   return (
-  	<div>
+  	<div>           
 	 	   <ProductList/>
 	 </div>
   );
