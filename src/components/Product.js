81d329942ef200f14ab1ba4b41a9c5294378ab4b
Chitipat Phetmunee <xtipat@gmail.com>
2019-05-29 12:59:11 +0900

    fix margin

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,47 +8,39 @@ export class Product extends Component {
 		const {id, title, img, price, inCart} = this.props.product;
 		return (
 
-			<ProductWrapper className="col-6">
-			<ProductConsumer>
-				{(value)=>(
-					<div className="card" 
-						onClick={() =>
-						value.handleDetail(id)
-						}
-					>
-						<Link to="/details">
-						<div className='reward-wrap'>
-							
-								
+			<ProductWrapper className="col-6 ">
+				<ProductConsumer>
+					{(value)=>(
+						<div className="card mb-2"  
+							onClick={() =>
+							value.handleDetail(id)
+							}
+						>
+							<Link className="product_link"  to="/details" >
+
+								<div className='reward-wrap'>
 									<div
 										//this is for making the image bigger
 										className="img-container p-6"
-										//onClick={() =>
-										//	value.handleDetail(id)
-										//}
-									>
-										
-											<img src={img} alt="product" className="card-img-top img-thumbnail img-responsive" />
-										
-
+									>	
+										<img src={img} alt="product" className="card-img-top img-thumbnail img-responsive" />
 									</div>
-								
-
+									{/*card footer */}
+									{/*card-footer from boostrap card*/}
+									<div className="card-footer d-flex justify-content-between ">
+										<p className="align-self-center mb-0 text-capitalize text-muted">
+											{title}
+										</p>
+										<h5 className="text-gold mb-0">
+											{price} points
+										</h5>
+									</div>
+								</div>
+							</Link>
 							
-							{/*card footer */}
-							<div className="card-footer d-flex justify-content-between ">
-								<p className="align-self-left mb-2 text-capitalize">
-									{title}
-								</p>
-								<h5 className="text-gold font-italic mb-0">
-									{price} points
-								</h5>
-							</div>
 						</div>
-						</Link>
-					</div>
-				)}
-			</ProductConsumer>
+					)}
+				</ProductConsumer>
 			</ProductWrapper>
 		);
 	}
@@ -57,13 +49,10 @@ export class Product extends Component {
 const ProductWrapper = styled.div`
 .card{
 	border-color:transparent;
-	transition:all 1s linear;
-
 }
 .card-footer{
 	background: transparent;
 	border-top: transparent;
-	transition: all 1s linear;
 }
 &:hover{
 	.card{
@@ -75,35 +64,23 @@ const ProductWrapper = styled.div`
 	}
 }
 .img-container{
-	position: relative;
 	overflow: hidden;
 }
 .card-img-top{
-	transition: all 1s linear;
+	transition: all 0.5s linear;
 }
-.img-container:hover .card-img-top{
-	transform: scale(1.2)
-}
-.cart-btn{
-	position: absolute;
-	bottom:0;
-	right:0;
-	padding:0.2rem 0.4rem;
-	background:var(--lightBlue);
-	border: none;
-	color: var(--mainWhite);
-	font-size:1.4rem;
-	border-radius: 0.5rem 0 0 0;
-	transform: translate(100%,100%);
-	transition: all 1s linear;
-}
+
+
 .img-container:hover .cart-btn{
 	transform: translate(0,0);
 }
-.cart-btn:hover {
-	color:var(--mainBlue);
-	cursor: pointer;
+.product_link:hover {
+   text-decoration: none; 
+   background: rgba(247, 247, 247);
+   colour: black
+
 }
+
 
 `
 
